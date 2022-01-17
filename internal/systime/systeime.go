714647a404585a5b4832d6bb2d8d03ca1fe25371
David Fryer <dfryer1193@gmail.com>
2022-01-17 15:41:26 -0500

    Implement interfaces for time block

--- internal/systime/systeime.go
+++ internal/systime/systeime.go
@@ -1,31 +1,54 @@
 package systime
 
 import (
+	"encoding/json"
 	"fmt"
 	"gobar/internal/blockutils"
+	"gobar/internal/log"
 	"time"
 )
 
-// GetTime returns a block containing the system time
-func GetTime(timeout time.Duration, blockCh chan<- *blockutils.Block) {
-	const clockSym = '\uf017'
-	timeBlock := blockutils.Block{
-		Name:        blockutils.TimeName,
-		Border:      blockutils.Blue,
-		BorderLeft:  0,
-		BorderRight: 0,
-		BorderTop:   0,
-		Urgent:      false,
-		FullText:    "",
+const name = blockutils.TimeName
+
+// Systime - a block showing the system time
+type Systime struct {
+	block *blockutils.Block
+}
+
+// NewSystime - Returns a new system time block
+func NewSystime() *Systime {
+	return &Systime{
+		block: &blockutils.Block{
+			Name:        name,
+			Border:      blockutils.Blue,
+			BorderLeft:  0,
+			BorderRight: 0,
+			BorderTop:   0,
+			Urgent:      false,
+			FullText:    "",
+		},
 	}
+}
+
+// Refresh - Refreshes the block containing the system time
+func (s *Systime) Refresh(timeout time.Duration) {
+	const clockSym = '\uf017'
 
 	for {
 		now := time.Now()
-		timeBlock.FullText = fmt.Sprintf("%s %02d.%02d.%02d", string(clockSym),
+		s.block.FullText = fmt.Sprintf("%s %02d.%02d.%02d", string(clockSym),
 			now.Hour(), now.Minute(), now.Second())
-
-		blockCh <- &timeBlock
 
 		time.Sleep(timeout)
 	}
+}
+
+// Marshal - Marshals the time block into json
+func (s *Systime) Marshal() []byte {
+	out, err := json.Marshal(s.block)
+	if err != nil {
+		log.FileLog(err)
+		return []byte("{}")
+	}
+	return out
 }
